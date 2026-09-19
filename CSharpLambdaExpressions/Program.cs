48416082cb7457ce6355caf9d02afd5b2ad95ffb
user-mosaad <msaadshahid999@gmail.com>
2026-09-19 18:32:41 +0600

    Write the code

--- CSharpLambdaExpressions/Program.cs
+++ CSharpLambdaExpressions/Program.cs
@@ -1,1 +1,27 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace CSharpLambdaExpressions;
+
+class Program
+{
+    delegate int Transform(int value);
+
+    static void Main()
+    {
+        Transform doubler = x => x * 2; // assign a lambda expression
+        Transform squarer = Square; // assign a named method
+
+        Console.WriteLine(doubler(5));
+        Console.WriteLine(squarer(5));
+
+        int left = 1;
+
+        // Using built-in delegate types
+        Func<int, int, int> add = static (left, right) => left + right;
+        Action<string> report = message => Console.WriteLine($"Report: {message}");
+
+        int total = add(5, 4); // 9
+        Console.WriteLine(total);
+        report("some message");
+    }
+
+    static int Square(int value) => value * value;
+}
